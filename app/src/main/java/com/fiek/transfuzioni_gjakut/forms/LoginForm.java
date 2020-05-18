0eecaf26a363bdafd77c65764c2f91d9128e149d
DiellzaBajrami <diellezabajrami@hotmail.com>
2020-05-19 01:19:49 +0200

    Initial commit

---
+++ app/src/main/java/com/fiek/transfuzioni_gjakut/forms/LoginForm.java
@@ -0,0 +1,16 @@
+package com.fiek.transfuzioni_gjakut.forms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fiek.transfuzioni_gjakut.R;
+
+public class LoginForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+}
---
+++ app/src/main/java/com/fiek/transfuzioni_gjakut/forms/RegistrationForm.java
@@ -0,0 +1,15 @@
+package com.fiek.transfuzioni_gjakut.forms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.fiek.transfuzioni_gjakut.R;
+
+
+public class RegistrationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+    }
+}
