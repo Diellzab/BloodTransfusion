d64dafcea991cb4ad41e26c0d2d06f641cda8d01
DiellzaBajrami <diellezabajrami@hotmail.com>
2020-06-15 23:45:42 +0200

    UI changes at registration form

--- app/src/main/java/com/fiek/transfuzioni_gjakut/activities/Registration_form.java
+++ app/src/main/java/com/fiek/transfuzioni_gjakut/activities/Registration_form.java
@@ -23,13 +23,13 @@ import com.fiek.transfuzioni_gjakut.forms.RegistrationForm;
 
 public class Registration_form extends AppCompatActivity {
 
-    TextView tvSignIn;
-    EditText etFName, etLName, etEmail, etPassword, etTelephone, etAddress;
-    RadioButton  radioButton;
-    Button btnRegister;
-    Spinner spinner;
-    CheckBox cbDonor;
-    RadioGroup radioGroup;
+//    TextView tvSignIn;
+//    EditText etFName, etLName, etEmail, etPassword, etTelephone, etAddress;
+//    RadioButton  radioButton;
+//    Button btnRegister;
+//    Spinner spinner;
+//    CheckBox cbDonor;
+//    RadioGroup radioGroup;
 
 
 
@@ -40,18 +40,18 @@ public class Registration_form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
 
-        etFName = findViewById(R.id.EditTextFirstName);
-        etLName =  findViewById(R.id.EditTextLastName);
-//        rbFemale =  findViewById(R.id.radioButtonFemale);
-//        rbMale =  findViewById(R.id.radioButtonMale);
-        etEmail = findViewById(R.id.EditTextEmailAddress);
-        etPassword =  findViewById(R.id.EditTextPassword);
-        etTelephone =  findViewById(R.id.EditTextTelephone);
-        etAddress =  findViewById(R.id.EditTextAddress);
-        btnRegister =  findViewById(R.id.buttonSave);
-        spinner =  findViewById(R.id.spinner);
-        cbDonor =  findViewById(R.id.checkBoxDonor);
-        radioGroup = findViewById(R.id.radioGroup);
+//        etFName = findViewById(R.id.EditTextFirstName);
+//        etLName =  findViewById(R.id.EditTextLastName);
+////        rbFemale =  findViewById(R.id.radioButtonFemale);
+////        rbMale =  findViewById(R.id.radioButtonMale);
+//        etEmail = findViewById(R.id.EditTextEmailAddress);
+//        etPassword =  findViewById(R.id.EditTextPassword);
+//        etTelephone =  findViewById(R.id.EditTextTelephone);
+//        etAddress =  findViewById(R.id.EditTextAddress);
+//        btnRegister =  findViewById(R.id.buttonSave);
+//        spinner =  findViewById(R.id.spinner);
+//        cbDonor =  findViewById(R.id.checkBoxDonor);
+//        radioGroup = findViewById(R.id.radioGroup);
 
 
 //        Spinner spinner = findViewById(R.id.spinner);
@@ -82,60 +82,58 @@ public class Registration_form extends AppCompatActivity {
 //            }
 //        });
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Registration_form.this, Login_form.class);
-                startActivity(intent);
-
-                int radioButtonId = radioGroup.getCheckedRadioButtonId();
-                radioButton = findViewById(radioButtonId);
-            }
-        });
-
-
-        tvSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login_form.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-
-
-    public  void SetValidation() {
-        //Check for a valid name
-
-        int radioId = radioGroup.getCheckedRadioButtonId();
-
-        if(radioId == -1){
-//            Toast.makeText(MainActivity.this, "Click one", Toast.LENGTH_SHORT).show();
-           return;
-        }
-        if(!cbDonor.isChecked()){
-//            Toast.makeText(MainActivity.this, "You should check this", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-//        Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onRadioButtonClicked(View v){
-
-        int radioButtonId = radioGroup.getCheckedRadioButtonId();
-        radioButton = findViewById(radioButtonId);
-        Toast.makeText(this, "Selected Blood Type: " + radioButton.getText(),
-                Toast.LENGTH_SHORT).show();
-
-    }
-
-
-
+//        btnRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(Registration_form.this, Login_form.class);
+//                startActivity(intent);
+//
+//                int radioButtonId = radioGroup.getCheckedRadioButtonId();
+//                radioButton = findViewById(radioButtonId);
+//            }
+//        });
+//
+//
+//        tvSignIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), Login_form.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//
+//
+//    public  void SetValidation() {
+//        //Check for a valid name
+//
+//        int radioId = radioGroup.getCheckedRadioButtonId();
+//
+//        if(radioId == -1){
+////            Toast.makeText(MainActivity.this, "Click one", Toast.LENGTH_SHORT).show();
+//           return;
+//        }
+//        if(!cbDonor.isChecked()){
+////            Toast.makeText(MainActivity.this, "You should check this", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+////        Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    public void onRadioButtonClicked(View v){
+//
+//        int radioButtonId = radioGroup.getCheckedRadioButtonId();
+//        radioButton = findViewById(radioButtonId);
+//        Toast.makeText(this, "Selected Blood Type: " + radioButton.getText(),
+//                Toast.LENGTH_SHORT).show();
+//
+//    }
+//
 
 
-    //Check for valid surname
+        //Check for valid surname
 
 
-}
+    }}
